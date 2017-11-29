6c373fba73e8e66ebda0f9d1174a0b2e8a51ec41
xgrg <goperto@gmail.com>
2017-11-30 00:35:25 +0100

    add test #2

--- tests/tests.py
+++ tests/tests.py
@@ -1,11 +1,17 @@
 import os, sys
 sys.path.append('./python')
-print sys.path
 import md_to_js
 
 def test_vallter():
     md_fp = './tests/vallter.md'
     json_fp = './tests/vallter.json'
     js_fp = './tests/vallter.js'
-    print 'toto'
+
     md_to_js.markdown_to_js(md_fp, json_fp, js_fp)
+
+def test_vallter2():
+    md_fp = './tests/vallter.md'
+    json_fp = './tests/vallter.json'
+    js_fp = './tests/vallter.js'
+
+    os.system('md_to_json %s %s 2'%(md_fp+'1', json_fp))

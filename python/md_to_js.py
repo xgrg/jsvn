1e897e768d045a76cc858402492f2edca9013d4a
xgrg <goperto@gmail.com>
2017-11-29 23:00:25 +0100

    comments

--- python/md_to_js.py
+++ python/md_to_js.py
@@ -3,6 +3,8 @@
 
 if __name__ == '__main__':
     import jsvn, json, argparse
+
+    # Parsing options
     parser = argparse.ArgumentParser(description='Converts Markdown code to Javascript\
         that can run as scenes', prog='md_to_js2',
         usage='%(prog)s markdown_file [json_file] [js_file]\n\n'\
@@ -12,20 +14,24 @@ if __name__ == '__main__':
     parser.add_argument('json', nargs='?', help='Json file')
     parser.add_argument('js', nargs='?', help='JS file')
     args = parser.parse_args()
+
     md_fp = args.md
     json_fp = args.json if not args.json is None else md_fp.replace('.md','.json')
     js_fp = args.js if not args.js is None else md_fp.replace('.md','.js')
 
     print md_fp, json_fp, js_fp
 
+    # Creates a new .md file without lines full of --------
+    # This new file is named [markdown_file].md1
     jsvn.remove_minuslines(md_fp)
 
+    # Creates a Json from Markdown
     from markdown_to_json.scripts import md_to_json
     md_to_json.jsonify_markdown(md_fp+'1', json_fp, 2)
+
+    # Generates JS from Json
     j = json.load(open(json_fp))
-
     js = jsvn.get_javascript(j)
-
     w = open(js_fp, 'w')
     w.write(js)
     w.close()

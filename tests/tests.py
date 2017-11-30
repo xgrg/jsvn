c9df94b3b39ccf6ca1f5b49fe22165e2f62d5383
xgrg <goperto@gmail.com>
2017-11-30 11:50:19 +0100

    Comments / renamed tests

--- tests/tests.py
+++ tests/tests.py
@@ -3,14 +3,17 @@ sys.path.append('./python')
 import md_to_js
 import jsvn
 
-def test_vallter():
+''' These tests are based on the Vallter scenes.
+The first ones check that the Markdown to JS conversion just gets to the end.'''
+
+def test_markdown_to_js():
     md_fp = './tests/vallter.md'
     json_fp = './tests/vallter.json'
     js_fp = './tests/vallter.js'
 
     md_to_js.markdown_to_js(md_fp, json_fp, js_fp)
 
-def test_vallter2():
+def test_jsonify_markdown():
     md_fp = './tests/vallter.md'
     json_fp = './tests/vallter.json'
     js_fp = './tests/vallter.js'

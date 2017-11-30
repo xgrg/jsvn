de80c06b49e0a2c91bd364d2f658ef878e5586be
xgrg <goperto@gmail.com>
2017-11-30 16:48:02 +0100

    Compiles every md on each commit (build.py / circleCI)

--- build.py
+++ build.py
@@ -2,8 +2,38 @@
 # prepare a folder build/ with everything
 import os
 import os.path as osp
+from glob import glob
 os.system('mkdir build/')
-os.system('cp -r images/ css/ js/ md/ index.html build/')
+os.system('cp -r images/ css/ js/ index.html md/ build/')
+
+
+# compile all md files in md:
+done = []
+errors = []
+md_files = glob('./build/md/*.md')
+for md in md_files:
+    print ""
+    print '###',md,'###'
+    json_fp = md.replace('.md', '.json')
+    js_fp = md.replace('.md', '.js')
+    cmd = 'python python/md_to_js.py %s %s %s'%(md, json_fp, js_fp)
+    print cmd
+    res = os.system(cmd)
+    if (res==0):
+        print md, 'converted successfully'
+        done.append(md)
+    else:
+        errors.append(md)
+
+os.system('rm build/md/*.json')
+os.system('rm build/md/*.md')
+
+# summary
+print '\n\n===================================================================='
+print 'The following files were correctly processed: \n%s'%', '.join(done)
+print '\nThe following files resulted with errors: \n%s'%', '.join(errors)
+print '========================================================================'
+
 
 # commit this folder to gh-pages
 basedir = os.curdir
